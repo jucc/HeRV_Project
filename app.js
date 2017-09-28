91a3f1c6c8eb64774fdd3d53866ffdd745ddb196
edulira <duslira@gmail.com>
2017-09-28 16:41:34 -0300

    Make some basic changes.

--- app.js
+++ app.js
@@ -21,17 +21,13 @@ function main()
     $('.app-back').on('click', function() { history.back() })
   })
 
-/*  // Event handler called when Cordova plugins have loaded.
+  // Event handler called when Cordova plugins have loaded.
   document.addEventListener(
     'deviceready',
     onDeviceReady,
     false)
-}*/
+}
 
-main()
-
-})();
-/*
 function onDeviceReady()
 {
   // Un-gray buttons.
@@ -46,6 +42,68 @@ function onDeviceReady()
   $('.app-start-scan').on('click', startScan)
   $('.app-stop-scan').on('click', stopScan)
 }
+
+function startScan()
+{
+  // Make sure scan is stopped.
+  stopScan()
+
+  // Start scan.
+  evothings.ble.startScan(
+    function(device)
+    {
+      // Device found. Sometimes an RSSI of +127 is reported.
+      // We filter out these values here.
+      if (device.rssi <= 0)
+      {
+        // Set timeStamp.
+        device.timeStamp = Date.now()
+
+        // Store device in table of found devices.
+        devices[device.address] = device
+      }
+    },
+    function(error)
+    {
+      showMessage('Scan error: ' + error)
+      stopScan()
+    }
+  )
+
+  // Start update timer.
+  updateTimer = setInterval(updateDeviceList, 500)
+
+  // Update UI.
+  $('.mdl-progress').addClass('mdl-progress__indeterminate')
+  showMessage('Scan started')
+}
+
+function stopScan()
+{
+  // Stop scan.
+  evothings.ble.stopScan()
+
+  // Clear devices.
+  devices = {}
+
+  // Stop update timer.
+  if (updateTimer)
+  {
+    clearInterval(updateTimer)
+    updateTimer = null
+  }
+
+  // Update UI.
+  $('.mdl-progress').removeClass('mdl-progress__indeterminate')
+  $('.app-cards').empty()
+  hideDrawerIfVisible()
+
+}
+
+main()
+
+})();
+/*
 
 function startScan()
 {
